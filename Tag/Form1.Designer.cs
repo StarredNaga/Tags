e0410c2440d342eda5376ddaf1b8258b8e95fe4c
MishaExper <122626471+MishaExper@users.noreply.github.com>
2024-07-21 17:07:19 +0700

    Добавьте файлы проекта.

---
+++ Tag/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MyWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(684, 561);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(230, 377);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(222, 181);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(3, 377);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(221, 181);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(458, 190);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(223, 181);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(230, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(222, 181);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(3, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(221, 181);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(458, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 181);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 181);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(230, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 181);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tag/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MyWinForms
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttons = null;
+        Button selectedButton = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+            selectedButton = button8;
+        }
+
+        private void StartGame()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            buttons = new List<Button>() { button1, button2, button3, button4, button5, button6, button7, button8 };
+            Random random = new Random();
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                int num = numbers[random.Next(numbers.Count)];
+                buttons[i].Text = num.ToString();
+
+                numbers.Remove(num);
+            }
+        }
+
+        private void CheckWin()
+        {
+            bool win = true;
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                Button current = buttons[i];
+
+                switch (current.Text)
+                {
+                    case "1":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(0, 0)) win = false;
+                        break;
+
+                    case "2":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(1, 0)) win = false;
+                        break;
+
+                    case "3":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(2, 0)) win = false;
+                        break;
+
+                    case "4":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(0, 1)) win = false;
+                        break;
+
+                    case "5":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(1, 1)) win = false;
+                        break;
+
+                    case "6":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(2, 1)) win = false;
+                        break;
+
+                    case "7":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(0, 2)) win = false;
+                        break;
+
+                    case "8":
+                        if (tableLayoutPanel1.GetPositionFromControl(current) != new TableLayoutPanelCellPosition(1, 2)) win = false;
+                        break;
+
+                }
+            }
+
+            if (!win) return;
+
+            MessageBox.Show("You Win!");
+            Application.Exit();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            selectedButton = (Button)sender;
+        }
+
+        private void Form1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            int buttonX = tableLayoutPanel1.GetColumn(selectedButton);
+            int buttonY = tableLayoutPanel1.GetRow(selectedButton);
+
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    y = -1;
+                    break;
+
+                case Keys.S:
+                    y = 1;
+                    break;
+
+                case Keys.D:
+                    x = 1;
+                    break;
+
+                case Keys.A:
+                    x = -1;
+                    break;
+            }
+
+            if (buttonX + x > 2 || buttonX + x < 0 || buttonY + y > 2 || buttonY + y < 0) return;
+
+            if (tableLayoutPanel1.GetControlFromPosition(buttonX + x, buttonY + y) == null)
+            {
+                try
+                {
+                    TableLayoutPanelCellPosition position = new TableLayoutPanelCellPosition(buttonX + x, buttonY + y);
+                    tableLayoutPanel1.SetCellPosition(selectedButton, position);
+                }
+                catch (Exception) { }
+
+                CheckWin();
+            }
+        }
+    }
+
+}
